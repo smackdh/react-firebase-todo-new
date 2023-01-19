50e27f8ed2d2e9403ea9c48eb3f043401d9848b5
Mattias Velamsson <mattiaswelamsson@gmail.com>
2023-01-19 17:43:04 +0900

    add firebase

---
+++ src/components/list/list.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Todo from "../todo/todo";
+
+const list = () => {
+  return <Todo />;
+};
+
+export default list;
---
+++ src/components/todo/todo.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+const todo = () => {
+  return (
+    <Fragment>
+      <div>
+        <h1>Task 1</h1>
+      </div>
+    </Fragment>
+  );
+};
+
+export default todo;
